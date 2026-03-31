896069e207d185a340741de32619e445e2a57f08
Yukkku <yukku.scratcher@gmail.com>
2026-03-31 22:27:50 +0900

    flake.nixを追加

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    {
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            name = "devshell";
+            packages = with pkgs; [
+              cargo
+              rustc
+              rust-analyzer
+              rustfmt
+              nixd
+              nixfmt
+            ];
+          };
+        }
+      );
+    };
+}
